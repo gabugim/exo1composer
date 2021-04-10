cc9e28c5f303f199f5157e94361bfda0be25d52c
Gaetan <gabu-g@hotmail.fr>
2021-04-10 18:30:43 +0200

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'gimat/exo1',
+  ),
+  'versions' => 
+  array (
+    'gimat/exo1' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
